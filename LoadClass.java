402f3e11fe17d2ca3a7bc0e7deae8f06a40866be
jbassi360 <57762609+jbassi360@users.noreply.github.com>
2019-11-14 10:01:35 -0500

    Add files via upload

---
+++ LoadClass.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.lang.*;
+
+public class LoadClass extends JFrame{
+    private JMenuBar menuBar;
+    private JButton button1;
+    private JComboBox combobox1;
+    private JLabel label1;
+
+    //Constructor
+    public LoadClass(){
+        File folder = new File("./txtFiles");
+        File[] files = folder.listFiles();
+
+        this.setTitle("Load a Class");
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        //menu generate method
+
+        this.setJMenuBar(menuBar);
+
+        //pane with null layout
+        JPanel contentPane = new JPanel(null);
+        JFrame frame = new JFrame();
+        frame.setPreferredSize( Toolkit.getDefaultToolkit().getScreenSize());
+        frame.setAlwaysOnTop(false);
+        frame.setVisible(true);
+        frame.setResizable(true);
+
+        button1 = new JButton();
+        button1.setBounds(296,234,90,35);
+        button1.setLocation(((((this.getWidth()-button1.getWidth())/2)+200)),((this.getHeight()/2)-100));
+        button1.setBackground(new Color(214,217,223));
+        button1.setForeground(new Color(0,0,0));
+        button1.setEnabled(true);
+        button1.setFont(new Font("sansserif",0,12));
+        button1.setText("Load");
+        button1.setVisible(true);
+
+
+        button1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                    try {
+                        if (combobox1.getSelectedIndex() < 0)
+                            throw new Exception();
+
+                        String name=combobox1.getSelectedItem().toString();
+
+                        File file = new File("./txtFiles/"+name+".txt");
+                        BufferedReader read = new BufferedReader(new FileReader(file));
+                        String students =read.readLine();
+                        String meetings =read.readLine();
+                        read.close();
+                        int numStudents = Integer.parseInt(students);
+                        int numMeetings = Integer.parseInt(meetings);
+                        new LoadedClass(name,numStudents,numMeetings);
+                        frame.dispose();
+
+                    }
+                    catch (Exception t){new Exceptions("You either forgot to save this file,\n you have not created your first file yet,\n or this file was saved empty.");}
+                }
+
+            });
+
+        JButton back = new JButton();
+        back.setBounds(200,334,90,35);
+        back.setLocation((((this.getWidth()-back.getWidth())/2)),((this.getHeight()/2)+100));
+        back.setBackground(new Color(214,217,223));
+        back.setForeground(new Color(0,0,0));
+        back.setEnabled(true);
+        back.setFont(new Font("sansserif",0,12));
+        back.setText("Back");
+        back.setVisible(true);
+        back.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    new MainMenu();
+                    frame.dispose();
+                }
+            });
+
+        String[] txtFiles=new String[0];
+        if(files!=null)
+        { txtFiles = new String[files.length];
+            for (int i=0;i<files.length;i++)
+            {
+                txtFiles[i]=files[i].getName().substring(0,files[i].getName().indexOf("."));
+            }
+            combobox1 = new JComboBox(txtFiles);}
+        else
+            combobox1 = new JComboBox();
+        combobox1.setBounds(91,233,120,35);
+        combobox1.setLocation(((((this.getWidth()-combobox1.getWidth())/2)-200)),((this.getHeight()/2)-100));
+        combobox1.setBackground(new Color(214,217,223));
+        combobox1.setForeground(new Color(0,0,0));
+        combobox1.setEnabled(true);
+        combobox1.setFont(new Font("sansserif",0,12));
+        combobox1.setVisible(true);
+
+        label1 = new JLabel();
+        label1.setBounds(139,85,230,53);
+        label1.setLocation((((this.getWidth()-label1.getWidth())/2)),(this.getHeight()/4));
+        label1.setBackground(new Color(214,217,223));
+        label1.setForeground(new Color(0,0,0));
+        label1.setEnabled(true);
+        label1.setFont(new Font("SansSerif",0,24));
+        label1.setText("Pick a class to load");
+        label1.setVisible(true);
+
+        //adding components to contentPane panel
+        contentPane.add(button1);
+        contentPane.add(back);
+        contentPane.add(combobox1);
+        contentPane.add(label1);
+        //frame.add(contentPane);
+
+        //adding panel to JFrame and seting of window position and close operation
+        frame.add(contentPane);
+        frame.setTitle("Student Grades");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    //method for generate menu
+
+
+    public static void main(String[] args){
+        System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    new LoadClass();
+                }
+            });
+    }
+
+}
+
---
+++ MainMenu.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends JFrame {
+
+    private JButton button1;
+    private JButton button2;
+    private JLabel label1;
+    private JLabel label2;
+
+    public MainMenu() {
+
+        this.setTitle("Grading");
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        //pane with null layout
+        JPanel contentPane = new JPanel(null);
+        JFrame frame = new JFrame();
+        frame.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.setAlwaysOnTop(false);
+
+        frame.setBackground(new Color(192, 192, 192));
+
+        button1 = new JButton();
+        button1.setBounds(SwingConstants.CENTER+200, SwingConstants.CENTER, 160, 46);
+        button1.setLocation((((this.getWidth()-button1.getWidth())/2)+200),(this.getHeight()-button1.getHeight())/2);
+        button1.setBackground(new Color(214, 217, 223));
+        button1.setForeground(new Color(0, 0, 0));
+        button1.setEnabled(true);
+        button1.setFont(new Font("sansserif", Font.PLAIN, 12));
+        button1.setText("New Class");
+        button1.setVisible(true);
+        button1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    new SetNewClass();
+                    frame.dispose();
+                }
+            });
+
+        button2 = new JButton();
+        button2.setBounds(SwingConstants.CENTER+200, SwingConstants.CENTER, 160, 46);
+        button2.setLocation((((this.getWidth()-button2.getWidth())/2)-200),(this.getHeight()-button2.getHeight())/2);
+        button2.setBackground(new Color(214, 217, 223));
+        button2.setForeground(new Color(0, 0, 0));
+        button2.setEnabled(true);
+        button2.setFont(new Font("sansserif", 0, 12));
+        button2.setText("Load Class");
+        button2.setVisible(true);
+        button2.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                    new LoadClass();
+
+                    frame.dispose();
+                }
+            });
+
+        label1 = new JLabel();
+        label1.setBounds(390, 99, 600, 33);
+        label1.setLocation((((this.getWidth()/2)-125)),(this.getHeight()/4));
+        label1.setBackground(new Color(214, 217, 223));
+        label1.setForeground(new Color(0, 0, 0));
+        label1.setEnabled(true);
+        label1.setFont(new Font("SansSerif", 0, 34));
+        label1.setText("Select an option");
+        label1.setVisible(true);
+
+        //adding components to contentPane panel
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(label1);
+
+        //adding panel to JFrame and seting of window position and close operation
+        frame.add(contentPane);
+        frame.setTitle("Student Grades");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
