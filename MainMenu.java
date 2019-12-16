d1e5cd997683ff15c9477eb2b034c0679b2a3a70
Natnael <57689834+NAhaile@users.noreply.github.com>
2019-12-16 11:17:57 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -2,35 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+/**
+ * Represents the Main menu class for our grading system.
+ * We use JFrame to help construct this.
+ */
+@SuppressWarnings("serial")
 public class MainMenu extends JFrame {
-
-    private JButton button1;
+	/**
+	 * The Main Menu has a couple of attributes
+	 * We initialize them being private.
+	 * Meaning they are accessible within the same class only.
+	 */
+	private JButton button1;
     private JButton button2;
     private JLabel label1;
-    private JLabel label2;
-
+    /**
+     * the constructor for the Main Menu
+     */
     public MainMenu() {
 
-        /*
-        * Sets title 
-        */
         this.setTitle("Grading");
         this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-
-        /*
-        * pane with null layout
-        */
+        /**
+         * the pane with the null layout
+         */
         JPanel contentPane = new JPanel(null);
         JFrame frame = new JFrame();
         frame.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
         frame.setAlwaysOnTop(false);
 
         frame.setBackground(new Color(192, 192, 192));
-        
-        /*
-        * creating buttons
-        */
+        /**
+         * the New Class Button
+         * Along with its functionalities
+         */
         button1 = new JButton();
         button1.setBounds(SwingConstants.CENTER+200, SwingConstants.CENTER, 160, 46);
         button1.setLocation((((this.getWidth()-button1.getWidth())/2)+200),(this.getHeight()-button1.getHeight())/2);
@@ -46,7 +51,10 @@ public class MainMenu extends JFrame {
                     frame.dispose();
                 }
             });
-
+        /**
+         * the Load Class Button
+         * Along with its functionalities
+         */
         button2 = new JButton();
         button2.setBounds(SwingConstants.CENTER+200, SwingConstants.CENTER, 160, 46);
         button2.setLocation((((this.getWidth()-button2.getWidth())/2)-200),(this.getHeight()-button2.getHeight())/2);
@@ -64,7 +72,9 @@ public class MainMenu extends JFrame {
                     frame.dispose();
                 }
             });
-
+        /**
+         * the Select an option Header
+         */
         label1 = new JLabel();
         label1.setBounds(390, 99, 600, 33);
         label1.setLocation((((this.getWidth()/2)-125)),(this.getHeight()/4));
@@ -74,17 +84,16 @@ public class MainMenu extends JFrame {
         label1.setFont(new Font("SansSerif", 0, 34));
         label1.setText("Select an option");
         label1.setVisible(true);
-
-        /*
-        *adding components to contentPane panel
-        */
+        /**
+         * adding the components to content pane panel
+         */
         frame.add(button1);
         frame.add(button2);
         frame.add(label1);
-
-        /*
-        *adding panel to JFrame and seting of window position and close operation
-        */
+        /**
+         * adding panel to JFrame and setting of the window position
+         * Closing operation
+         */
         frame.add(contentPane);
         frame.setTitle("Student Grades");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
