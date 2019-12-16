810fa358f875810723470caa443ffa250113de53
Natnael <57689834+NAhaile@users.noreply.github.com>
2019-12-16 11:20:13 -0500

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,6 +1,17 @@
 public class Start {
-
+	/**
+	 * The Start class simply implements methods from the Main Menu class
+	 * @author Tannon
+	 * @author Natnael
+	 * @author Jaspreet
+	 * @author Mohamed
+	 * @version 1.0
+	 * @since 2019-12-15
+	 */
     public static void main(String[] args) {
+    	/**
+    	 * runs the program from the Main Menu Class.
+    	 */
         new MainMenu();
     }
 }
